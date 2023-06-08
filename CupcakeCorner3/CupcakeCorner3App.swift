964abf044c718879b1cfb2c9e2fd505969aebfeb
pegahtafviziingoampt <ingoampt@gmail.com>
2023-06-08 14:01:08 +0200

    Initial Commit

---
+++ CupcakeCorner3/CupcakeCorner3App.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCorner3App.swift
+//  CupcakeCorner3
+//
+//  Created by Pegah Tafvizi on 08.06.23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCorner3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
